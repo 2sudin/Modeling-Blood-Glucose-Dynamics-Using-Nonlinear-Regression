d98b534d80a417a87d402be0bb336ef39ea05851
Sudin Maharjan <mail2.inertguy@gmail.com>
2025-12-18 21:02:29 +0545

    Add data visualization

--- blood-glucose-modeling.R
+++ blood-glucose-modeling.R
@@ -61,3 +61,34 @@ names(data)[names(data) %in% c("bg_mean","insulin_sum","carbs_sum","hr_mean","st
 
 # Reordering columns for standard convention
 data <- data[, c("time", "x1", "x2", "x3", "x4", "x5", "x6", "Y")]
+
+# Task 2: Data Visualization & Exploratory Analysis
+
+# Melt data for ggplot time series
+ts_data_subset <- data %>% dplyr::select(time, x1, x2, x3, x4, x5, x6)
+ts_long <- melt(ts_data_subset, id.vars = "time")
+
+var_labels <- c(x1="bg_mean", x2="insulin_sum", x3="carbs_sum", x4="hr_mean", x5="steps_sum", x6="cals_sum")
+var_colors <- c(x1="#F4A3A3", x2="#FFD966", x3="#8FD694", x4="#87CEEB", x5="#4A90E2", x6="#F4A6C1")
+
+# Faceted time series plots
+ggplot(ts_long, aes(x = time, y = value, color = variable)) +
+  geom_line(linewidth = 0.4) +
+  facet_wrap(~variable, scales = "free_y", ncol = 1, 
+             labeller = labeller(variable = function(x) paste(x, "-", var_labels[x]))) +
+  scale_color_manual(values = var_colors) +
+  labs(title = "Time Series Trends", x = "Time", y = "Value") +
+  theme_minimal() + theme(legend.position = "none")
+
+# Distributions with Skewness annotation
+skew_df <- ts_long %>%
+  group_by(variable) %>%
+  summarise(skewness = round(skewness(value, na.rm = TRUE), 3))
+
+ggplot(ts_long, aes(x = value, fill = variable)) +
+  geom_histogram(aes(y = after_stat(density)), bins = 50, alpha = 0.6, color = "black") +
+  geom_density(alpha = 0.3, linewidth = 1) +
+  facet_wrap(~variable, scales = "free", ncol = 3) +
+  geom_text(data = skew_df, aes(x = Inf, y = Inf, label = paste("Skew:", skewness)),
+            hjust = 1.1, vjust = 1.5, size = 3, inherit.aes = FALSE) +
+  theme_minimal()
